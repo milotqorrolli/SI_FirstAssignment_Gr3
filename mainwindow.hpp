ecc7fe3c6a0530594d53b35292949227defaebe6
Milot Qorrolli <118934072+milotqorrolli@users.noreply.github.com>
2025-10-19 21:11:06 +0200

    Finished UI header

---
+++ mainwindow.hpp
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QFileDialog>
+#include <QMessageBox>
+#include "pdf_encryptor.hpp"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onEncryptClicked();
+    void onDecryptClicked();
+    void onSelectInputFile();
+    void onSelectOutputFile();
+
+private:
+    Ui::MainWindow *ui;
+    PDFEncryptor* pdfEncryptor;
+
+    QString inputFilePath;
+    QString outputFilePath;
+    QLineEdit* keyInput;
+    QLabel* inputLabel;
+    QLabel* outputLabel;
+    QPushButton* encryptButton;
+    QPushButton* decryptButton;
+    QPushButton* selectInputButton;
+    QPushButton* selectOutputButton;
+};
+
+#endif 
